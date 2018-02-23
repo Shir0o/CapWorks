ff4b3e7f03472fd0d6c5a2cca02d5ebea691061b
Shir0o <wangy57@vcu.edu>
2018-02-23 10:55:46 -0500

    object_array

--- class/caps.class.php
+++ class/caps.class.php
@@ -2,11 +2,7 @@
 
 class Caps {
 
-  static public $database;
-
-  static public function set_database($database) {
-    self::$database = $database;
-  }
+  static protected $database;
 
   static protected $table_name = 'caps';
   static protected $db_columns = ['id', 'brewer', 'name', 'type', 'country', 'color', 'othercolor', 'textcolor', 'rimcolor', 'text', 'opening'];
@@ -23,6 +19,40 @@ class Caps {
   public $text;
   public $opening;
 
+  static public function set_database($database) {
+    self::$database = $database;
+  }
+
+  static public function find_by_sql($sql) {
+    $result = self::$database->query($sql);
+    if(!$result) exit("Query failed");
+
+    $object_array = [];
+    while($record = $result->fetch_assoc()) {
+      $object_array[] = self::instantiate($record);
+    }
+
+    $result->free();
+
+    return $object_array;
+  }
+
+  static protected function instantiate($record) {
+    $object = new self;
+    foreach ($record as $property => $value) {
+      if(property_exists($object, $property)) {
+        $object->$property = $value;
+      }
+    }
+
+    return $object;
+  }
+
+  static public function find_all() {
+    $sql = "SELECT * FROM caps";
+
+    return self::find_by_sql($sql);
+  }
 }
 
  ?>
--- index.php
+++ index.php
@@ -1,18 +1,38 @@
 <?php require_once('initialize.php'); ?>
 
+<table id="inventory">
+  <tr>
+    <th>ID</th>
+    <th>Brewer</th>
+    <th>Name</th>
+  </tr>
+
 <?php
 
-$sql = "SELECT * FROM caps";
-$result = Caps::$database->query($sql);
+$caps = Caps::find_all();
 
-if ($result->num_rows > 0) {
-    echo "<table><tr><th>ID</th><th>Brewer</th><th>Name</th></tr>";
-    while($row = $result->fetch_assoc()) {
-        echo "<tr><td>".$row["id"]."</td><td>".$row["brewer"]."</td><td>".$row["name"]."</td></tr>";
-    }
-    echo "</table>";
-} else {
-    echo "0 results";
-}
+?>
 
- ?>
+  <?php foreach ($caps as $cap) { ?>
+    <tr>
+      <td><?php echo h($cap->id); ?></td>
+      <td><?php echo h($cap->brewer); ?></td>
+      <td><?php echo h($cap->name); ?></td>
+    </tr>
+  <?php } ?>
+
+</table>
+
+<?php
+
+// if ($result->num_rows > 0) {
+//     echo "<table><tr><th>ID</th><th>Brewer</th><th>Name</th></tr>";
+//     while($row = $result->fetch_assoc()) {
+//         echo "<tr><td>".$row["id"]."</td><td>".$row["brewer"]."</td><td>".$row["namename"]."</td></tr>";
+//     }
+//     echo "</table>";
+// } else {
+//     echo "0 results";
+// }
+
+?>
